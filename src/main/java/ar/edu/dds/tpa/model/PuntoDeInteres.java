dcbc2ab0eb3b3e8dff178a33fbc3f3605b3383bd
Ronan Camargo <ronan.cam@gmail.com>
2016-04-20 19:37:33 -0300

    Agrego interfaz para manejar los servicios de los CGP

--- src/main/java/ar/edu/dds/tpa/model/PuntoDeInteres.java
+++ src/main/java/ar/edu/dds/tpa/model/PuntoDeInteres.java
@@ -3,11 +3,13 @@ package ar.edu.dds.tpa.model;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 public abstract class PuntoDeInteres {
 	private String nombre;
 	private Direccion direccion;
 	private Geolocalizacion geolocalizacion;
 	private ArrayList<Date> horariosDeAtencion = new ArrayList<Date>();
+
 		
 	public String getNombre() {
 		return nombre;

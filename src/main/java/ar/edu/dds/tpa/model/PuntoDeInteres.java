4697a1f698967077d541d8e887b32d01b6244d3d
Federico Fernandez <fede.a25@gmail.com>
2016-04-22 11:05:03 -0300

    YAGNI (for the moment :P)

--- src/main/java/ar/edu/dds/tpa/model/PuntoDeInteres.java
+++ src/main/java/ar/edu/dds/tpa/model/PuntoDeInteres.java
@@ -1,33 +1,10 @@
 package ar.edu.dds.tpa.model;
 
-import java.util.ArrayList;
-import java.util.Date;
-
-
 public abstract class PuntoDeInteres {
 	private String nombre;
-	private Direccion direccion;
-	private Geolocalizacion geolocalizacion;
-	private ArrayList<Date> horariosDeAtencion = new ArrayList<Date>();
-
 		
 	public String getNombre() {
 		return nombre;
 	}
 
-	public Direccion getDireccion() {
-		return direccion;
-	}
-
-	public Geolocalizacion getGeolocalizacion() {
-		return geolocalizacion;
-	}
-
-	public ArrayList<Date> getHorariosDeAtencion() {
-		return horariosDeAtencion;
-	}
-
-	public void agregarHorarioDeAtencion(Date horarioDeAtencion) {
-		this.horariosDeAtencion.add(horarioDeAtencion);
-	}
 }

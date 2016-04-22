c71cab697f0b2aa0ebe39e232469a47c0fcf012e
Federico Fernandez <fede.a25@gmail.com>
2016-04-22 14:52:35 -0300

    YAGNI, nuevo diseño, calculos de cercania y de disponibilidad, faltan MUCHOS tests

--- src/main/java/ar/edu/dds/tpa/model/ParadaDeColectivo.java
+++ src/main/java/ar/edu/dds/tpa/model/ParadaDeColectivo.java
@@ -1,5 +1,21 @@
 package ar.edu.dds.tpa.model;
 
+import java.time.LocalDateTime;
+
+import org.uqbar.geodds.Point;
+
 public class ParadaDeColectivo extends PuntoDeInteres {
 
+	public ParadaDeColectivo(String nombre, Point coordenadas) {
+		super(nombre, coordenadas);
+	}
+	
+	@Override
+	public boolean estaCercaDe(Point unaPosicion) {
+		return this.getCoordenadas().distance(unaPosicion) <= 0.1;
+	}
+	
+	public boolean estaDisponibleEn(LocalDateTime unDiaYHorario) {
+		return true;
+	}
 }
--- src/main/java/ar/edu/dds/tpa/model/PuntoDeInteres.java
+++ src/main/java/ar/edu/dds/tpa/model/PuntoDeInteres.java
@@ -1,10 +1,25 @@
 package ar.edu.dds.tpa.model;
 
+import org.uqbar.geodds.Point;
+
 public abstract class PuntoDeInteres {
 	private String nombre;
-		
+	private Point coordenadas;
+
+	public PuntoDeInteres(String nombre, Point coordenadas) {
+		this.nombre = nombre;
+		this.coordenadas = coordenadas;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
-
+	
+	public Point getCoordenadas() {
+		return coordenadas;
+	}
+	
+	public boolean estaCercaDe(Point unaPosicion){
+		return this.coordenadas.distance(unaPosicion) <= 0.5;				
+	}
 }

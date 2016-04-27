8dafbf96c103ae7a2e05ec52d1d9b61d869f75ae
Emi1305 <edelpeon@gmail.com>
2016-04-27 11:11:55 -0300

    Saque los if que estaban en la clase HorarioDeAtencion para hacerlo mas objetoso

--- src/main/java/ar/edu/dds/tpa/model/HorarioDeAtencion.java
+++ src/main/java/ar/edu/dds/tpa/model/HorarioDeAtencion.java
@@ -9,17 +9,14 @@ public class HorarioDeAtencion {
 	private Hashtable<DayOfWeek, ArrayList<RangoDeHorario>> horarioSemanal = new Hashtable<DayOfWeek, ArrayList<RangoDeHorario>>();
 
 	public void agregarRangoDeHorario(DayOfWeek dia, RangoDeHorario rangoDeHorario) {
-		if (!horarioSemanal.containsKey(dia)){ // Si todavia no estaba el dia entre los horarios lo agrego
-			horarioSemanal.put(dia, new ArrayList<RangoDeHorario>());
-		}
+		
+		horarioSemanal.putIfAbsent(dia, new ArrayList<RangoDeHorario>());// Si todavia no estaba el dia entre los horarios lo agrego
 		horarioSemanal.get(dia).add(rangoDeHorario);
 	}
 
 	public boolean estaDentroDeRangosDeHorario(LocalDateTime diaYHora) {
-		if (!horarioSemanal.containsKey(diaYHora.getDayOfWeek())){
-			return false;
-		}
-		return horarioSemanal.get(diaYHora.getDayOfWeek())
+		return horarioSemanal.containsKey(diaYHora.getDayOfWeek()) &&
+				horarioSemanal.get(diaYHora.getDayOfWeek())
 				.stream()
 				.anyMatch(rango -> rango.estaDentroDelRangoDeHorario(diaYHora.toLocalTime()));
 	}

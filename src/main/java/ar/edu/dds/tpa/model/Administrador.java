4ce6185228d716cff1dccaafb880b419740f7f0d
CamargoR <ronan.cam@gmail.com>
2016-06-01 20:31:14 -0300

    Item 2b (falta terminar)

---
+++ src/main/java/ar/edu/dds/tpa/model/Administrador.java
@@ -0,0 +1,11 @@
+package ar.edu.dds.tpa.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Administrador {
+	
+	
+	List<Terminal> terminales = new ArrayList<Terminal>();
+
+}

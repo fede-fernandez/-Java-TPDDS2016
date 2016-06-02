e7ceb156c1cdbb64255d7e3770a0a6a254d59c0b
Federico Fernandez <fede.a25@gmail.com>
2016-06-02 13:53:55 -0300

    Agrego jersey y gson al pom, comienzo con servicios. Agrego ABMC a mapa

--- src/main/java/ar/edu/dds/tpa/model/Mapa.java
+++ src/main/java/ar/edu/dds/tpa/model/Mapa.java
@@ -15,6 +15,15 @@ public class Mapa {
 	public void agregarPuntoDeInteres(PuntoDeInteres unPuntoDeInteres) {
 		puntosDeInteres.add(unPuntoDeInteres);
 	}
+	
+	public void sacarPuntoDeInteres(PuntoDeInteres unPuntoDeInteres) {
+		puntosDeInteres.remove(unPuntoDeInteres);
+	}
+	
+	public void modificarPuntoDeInteres(PuntoDeInteres unPuntoDeInteres, PuntoDeInteres puntoDeInteresModificado) {
+		puntosDeInteres.remove(unPuntoDeInteres);
+		puntosDeInteres.add(puntoDeInteresModificado);
+	}
 
 	public List<PuntoDeInteres> buscarPorTextoLibre(String unaFrase) {
 		List<String> palabrasClave = Arrays.asList(unaFrase.split(" "));
---
+++ src/main/java/ar/edu/dds/tpa/services/BancoServiceImpl.java
@@ -0,0 +1,7 @@
+package ar.edu.dds.tpa.services;
+
+import javax.ws.rs.GET;
+
+public class BancoServiceImpl {
+
+}

7b48e343c61c74c070346f51b7830f0f22f94730
Federico Fernandez <fede.a25@gmail.com>
2016-06-02 14:48:15 -0300

    Armo el adapter de bancos

--- src/main/java/ar/edu/dds/tpa/adapter/BancoServiceAdapter.java
+++ src/main/java/ar/edu/dds/tpa/adapter/BancoServiceAdapter.java
@@ -1,5 +1,62 @@
 package ar.edu.dds.tpa.adapter;
 
-public class BancoServiceAdapter {
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import ar.edu.dds.tpa.geolocalizacion.Posicion;
+import ar.edu.dds.tpa.model.Banco;
+import ar.edu.dds.tpa.model.Servicio;
+
+public class BancoServiceAdapter {
+	public List<Banco> obtenerBancosDeJSON(String bancosEnJSON) {
+		List<Banco> bancos = new ArrayList<Banco>();
+		Type listaDeBancos = new TypeToken<List<BancoDelServicio>>() {}.getType();
+		Gson gson = new Gson();
+		gson.fromJson(bancosEnJSON, listaDeBancos);
+		
+		return bancos;
+	}
+	
+	public Banco parsearBancoDelServicioABanco (BancoDelServicio bancoDelServicio) {
+		Banco banco = new Banco(bancoDelServicio.getBanco(), new Posicion(bancoDelServicio.getX(), bancoDelServicio.getY()));
+		bancoDelServicio.getServicios().stream().forEach(unServicio -> banco.agregarServicio(new Servicio(unServicio)));
+		return banco;
+	}
+}
+
+class BancoDelServicio {
+	private String banco;
+	private double x;
+	private double y;
+	private String sucursal;
+	private String gerente;
+	private List<String> servicios;
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public String getSucursal() {
+		return sucursal;
+	}
+
+	public String getGerente() {
+		return gerente;
+	}
+
+	public List<String> getServicios() {
+		return servicios;
+	}
 }
--- src/main/java/ar/edu/dds/tpa/services/BancoServiceImpl.java
+++ src/main/java/ar/edu/dds/tpa/services/BancoServiceImpl.java
@@ -2,6 +2,11 @@ package ar.edu.dds.tpa.services;
 
 import javax.ws.rs.GET;
 
-public class BancoServiceImpl {
-
+public class BancoServiceImpl implements BancoService{
+	@GET
+	@Override
+	public String getSucursalesBancosByNombreBanco(String nombreBanco) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

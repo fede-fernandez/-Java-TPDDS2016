b1fe57f54f65c4a9aabe41ebfe3111ebd0d7ae87
Federico Fernandez <fede.a25@gmail.com>
2016-06-29 14:38:46 -0300

    Agrego test de ejecutar proceso a un horario dado

--- src/main/java/ar/edu/dds/tpa/model/Usuario.java
+++ src/main/java/ar/edu/dds/tpa/model/Usuario.java
@@ -3,8 +3,11 @@ package ar.edu.dds.tpa.model;
 import ar.edu.dds.tpa.geolocalizacion.Posicion;
 
 public class Usuario {
+	String nombre;
 	Posicion coordenadas;
-	public Usuario(Posicion coordenadas) {
+	
+	public Usuario(String nombre, Posicion coordenadas) {
+		this.nombre = nombre;
 		this.coordenadas = coordenadas;
 	}
 }

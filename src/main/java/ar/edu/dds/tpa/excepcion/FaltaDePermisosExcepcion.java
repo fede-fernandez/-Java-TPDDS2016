1dad8c37b2ea2f30b41d6e2c6585959538bbf0d3
CamargoR <ronan.cam@gmail.com>
2016-06-30 00:21:03 -0300

    Elimino clase que maneja excepcion

--- src/main/java/ar/edu/dds/tpa/excepcion/FaltaDePermisosExcepcion.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.dds.tpa.excepcion;
-
-public class FaltaDePermisosExcepcion extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public FaltaDePermisosExcepcion(String mensaje) {
-		super(mensaje);
-	}
-}

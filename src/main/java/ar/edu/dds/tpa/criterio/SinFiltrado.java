023695ec195f53942ae9456d635c04a41c10df61
CamargoR <ronan.cam@gmail.com>
2016-06-29 22:52:41 -0300

    Mejoro criterios

--- src/main/java/ar/edu/dds/tpa/criterio/SinFiltrado.java
+++ src/main/java/ar/edu/dds/tpa/criterio/SinFiltrado.java
@@ -1,12 +1,11 @@
 package ar.edu.dds.tpa.criterio;
 
-import java.util.List;
-
 import ar.edu.dds.tpa.model.Usuario;
+
 
 public class SinFiltrado implements Criterio{
 	
-	public List<Usuario> filtrarUsuarios(List<Usuario> usuarios){
-		return usuarios;
+	public boolean filtrarUsuarios(Usuario usuario){
+		return true;
 	}
 }

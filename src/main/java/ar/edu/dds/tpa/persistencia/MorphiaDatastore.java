675380e11f4d6f62c1ad42f05e971337704edbfe
CamargoR <ronan.cam@gmail.com>
2016-10-16 22:20:16 -0300

    Agrego singleton para el datastore de morphia

---
+++ src/main/java/ar/edu/dds/tpa/persistencia/MorphiaDatastore.java
@@ -0,0 +1,37 @@
+package ar.edu.dds.tpa.persistencia;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import com.mongodb.MongoClient;
+
+import ar.edu.dds.tpa.model.Busqueda;
+import ar.edu.dds.tpa.model.HistorialDeBusqueda;
+import ar.edu.dds.tpa.properties.Propiedades;
+
+public class MorphiaDatastore {
+	
+	private Morphia morphia;
+	private Datastore datastore;
+	private Propiedades propiedades;
+	private static MorphiaDatastore morphiaDatastore;
+	
+	private MorphiaDatastore(){
+		propiedades = new Propiedades("resources/mongo.properties");
+		morphia = new Morphia();
+		morphia.map(Busqueda.class,HistorialDeBusqueda.class);	
+		datastore = morphia.createDatastore(new MongoClient(), propiedades.obtenerValorDe("nombreDeBD"));		
+	}
+	
+	public static MorphiaDatastore obtenerInstancia(){
+		if (morphiaDatastore == null) {
+			morphiaDatastore = new MorphiaDatastore();
+		}
+		return morphiaDatastore;
+	}
+	
+	public Datastore getDatastore(){
+		return datastore;
+	}
+
+}
